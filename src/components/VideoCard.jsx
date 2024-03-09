de5012be9dcbbc13bbe02cd3794623f20c093b43
prakash-joshi <prakash.joshi167@hotmail.com>
2024-03-09 21:21:18 +0530

    add details to video card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,10 +1,28 @@
 const VideoCard = (props) => {
-  const { snippet } = props?.info;
-  const { thumbnails } = snippet;
+  const { contentDetails, snippet, statistics } = props?.info;
+  const { thumbnails, title, channelId, channelTitle } = snippet;
+  const { viewCount } = statistics;
+  const { duration } = contentDetails;
+
+  const truncateTitle = (title) => {
+    if (title.length > 40) {
+      return title.slice(0, 35) + "...";
+    } else {
+      return title;
+    }
+  };
 
   return (
     <div className="relative my-2">
-      <img src={thumbnails?.medium?.url} alt="video" className="rounded-xl" />
+      <img src={thumbnails?.medium?.url} alt="thumbnail" className="rounded-xl" />
+      {/* <span className="absolute bottom-1 right-1 bg-black text-xs font-medium px-1 py-[2px] rounded-lg text-cyan-100">
+        {duration}
+      </span> */}
+      <div className="text-wrap">
+        <p>{truncateTitle(title)}</p>
+        <p>{"channelTitle : " + channelTitle}</p>
+        <p>{"views : " + viewCount}</p>
+      </div>
     </div>
   );
 };
